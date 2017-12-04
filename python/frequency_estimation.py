740e12c00acd501dda29aff8301b1e8e0f24444a
alrehn <alrm014@gmail.com>
2017-12-03 19:04:25 -0800

    Adjust parameters and fix for accuracy

--- python/frequency_estimation.py
+++ python/frequency_estimation.py
@@ -4,79 +4,48 @@ import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import wave
 import struct
+import time
+import queue
+import pylab
 from scipy.io.wavfile import read as wavread
-
-allSamples = np.array()
-
-
-
-mapping = [c - 1 for c in args.channels]  # Channel numbers start with 1
-# q = queue.Queue()
-
-count =  0
-def audio_callback(indata, frames, Atime, status):
-    """This is called (from a separate thread) for each audio block."""
-    global count
-    print("count", count, "length", len(indata))
-    count += 1
-    if status:
-        print(status, file=sys.stderr)
-    for item in np.ravel(indata):
-      aFile.write("%s\n" % item)
-    time.sleep(2)
-    # aFile.write(np.ravel(indata))
-    # Fancy indexing with mapping creates a (necessary!) copy:
-    # q.put(indata[::args.downsample, mapping])
-
-
-
-try:
-    from matplotlib.animation import FuncAnimation
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import sounddevice as sd
-
-    if args.list_devices:
-        print(sd.query_devices())
-        parser.exit(0)
-    if args.samplerate is None:
-        device_info = sd.query_devices(args.device, 'input')
-        args.samplerate = device_info['default_samplerate']
-
-    length = int(args.window * args.samplerate / (1000 * args.downsample))
-    plotdata = np.zeros((length, len(args.channels)))
-
-    # fig, ax = plt.subplots()
-    # lines = ax.plot(plotdata)
-    # if len(args.channels) > 1:
-    #     ax.legend(['channel {}'.format(c) for c in args.channels],
-    #               loc='lower left', ncol=len(args.channels))
-    # ax.axis((0, len(plotdata), -1, 1))
-    # ax.set_yticks([0])
-    # ax.yaxis.grid(True)
-    # ax.tick_params(bottom='off', top='off', labelbottom='off',
-                   # right='off', left='off', labelleft='off')
-    # fig.tight_layout(pad=0)
-
-    stream = sd.InputStream(
-        device=args.device, channels=max(args.channels),
-        samplerate=args.samplerate, callback=audio_callback)
-
-    # ani = FuncAnimation(fig, interval=args.interval, blit=True)
-    # with stream:
-        # pass
-
-except Exception as e:
-    parser.exit(type(e).__name__ + ': ' + str(e))
-
-
-
-
 
 fig=plt.figure(figsize=(6,3))
 fs = 44100 # sample rate
 x = numpy.arange(fs)
 frameNum = 2048
+
+amp_threshold = 275
+min_real_freq = 80
+AMP_DELAY = 2
+
+channels = [1]
+downsample = 1
+window = 200
+mapping = [c - 1 for c in channels]  # Channel numbers start with 1
+q = queue.Queue()
+
+count =  0
+def audio_callback(indata, frames, Atime, status):
+    """This is called (from a separate thread) for each audio block."""
+    global count
+    # print("count", count, "length", len(indata))
+    count += 1
+    # if status:
+    #     print(status)
+    #for item in np.ravel(indata):
+    #  aFile.write("%s\n" % item)
+    # aFile.write(np.ravel(indata))
+    # Fancy indexing with mapping creates a (necessary!) copy:
+    q.put(indata[::downsample, mapping])
+
+from matplotlib.animation import FuncAnimation
+import sounddevice as sd
+
+length = int(window * fs / (1000 * downsample))
+plotdata = numpy.zeros((length, len(channels)))
+stream = sd.InputStream(
+    device=5,channels=max(channels),
+    samplerate=fs, callback=audio_callback)
 
 def sinWave(frequency, amplitude, phase=0, duration = 1.0):
     sNum = fs * duration
@@ -123,6 +92,7 @@ def readWavSamples(filename):
     return samples
 
 def autoCorFreqEstimateOverTime(samples):
+    # print("test2: ", len(samples))
     peaks = numpy.zeros(len(samples)//frameNum + 1)
     i = 0
     cur = 0
@@ -132,7 +102,7 @@ def autoCorFreqEstimateOverTime(samples):
       acor = getAutoCorellation(workingFrames, frameNum)
       #return acor
       peaks[i] = getMaxPeak(acor)
-      print("acor peak: ",peaks[i])
+      # print("acor peak: ",peaks[i])
       cur += frameNum
       i += 1
     newPeaks = 44100 / peaks
@@ -149,7 +119,7 @@ def amplitudeOverTime(samples):
     while cur < len(samples):
       workingFrames = samples[cur:cur+frameNum]
       amplitudes[i] = peakRMS(workingFrames)
-      print("amp: ", amplitudes[i])
+      # print("amp: ", amplitudes[i])
       cur += frameNum
       i += 1
 
@@ -163,6 +133,7 @@ def smoothFrequencies(frequencies, threshold = 3, deviation_threshold = 10):
   idx = 0
   idx_jump = 1
   freqs = numpy.copy(frequencies)
+  # print("test: ", len(freqs))
 
   while(idx < len(freqs)):
     sum = 0
@@ -179,16 +150,16 @@ def smoothFrequencies(frequencies, threshold = 3, deviation_threshold = 10):
         freq = freqs[idx+j]
         deviation += (avg - freq) * (avg - freq)
     std_deviation = numpy.sqrt(deviation / threshold)
-    print("place: ", idx)
+    # print("place: ", idx)
     if std_deviation < deviation_threshold:
         if idx+j >= len(freqs):
             break
-        print("test1 freq: ", freqs[idx+j])
+        # print("test1 freq: ", freqs[idx+j])
         for j in range(0,threshold):
             freqs[idx+j] = avg
         idx += threshold
     else:
-        print("test2: ", std_deviation, " and " , deviation)
+        # print("test2: ", std_deviation, " and " , deviation)
         for j in range(0,idx_jump):
             if idx+j >= len(freqs):
                 break
@@ -196,70 +167,128 @@ def smoothFrequencies(frequencies, threshold = 3, deviation_threshold = 10):
         idx += idx_jump
   return freqs
 
-
-
-fname = '/home/alrehn/dev/guitartosheet/pianoCMajor.wav'
-
-audio_samples = readWavSamples(fname)
-
-freqs = autoCorFreqEstimateOverTime(audio_samples)
-
-plt.plot(freqs)
-
-freqs = smoothFrequencies(freqs)
-
-plt.plot(freqs)
-
-amps = amplitudeOverTime(audio_samples)
-
-plt.plot(amps)
-
-plt.savefig('freqs.png')
-
-print(len(freqs))
-print(len(amps))
-
 ## Sudden amplitude peak == start of a note. Match that with the nearest detected frequency = note detected
-n_cnt = 0
-amp_threshold = 30
-min_real_freq = 80
-peak_amp = 0
-delay = 3
-i = delay
-notes = []
-while i < len(amps):
-  if(amps[i] < peak_amp/25):
-    # end note
-    print("note end: ", i)
+def getNotes(freqs, amps, start_time):
+    n_cnt = 0
     peak_amp = 0
-    notes[-1].append(i-notes[-1][0])
-  diff = amps[i] - amps[i-delay]
-  if(diff > amp_threshold):
-    n_cnt += 1
-    if(peak_amp > 0):
-      # end previous note if it's there
-      notes[-1].append(i-notes[-1][0])
-      peak_amp = 0
-    peak_amp = amps[i]
-    #hit start of note
-    #print("note start: ",i)
-    j = min(i + 8,len(amps)-1)
-    while(freqs[j] < min_real_freq):
-      if j >= len(amps)-1:
-          break;
-      j += 1
-    freq = freqs[j]
-    #print("note frequency: ",freqs[j])
-    i += delay
-    notes.append([i, freqs[j]])
-  i += 1
+    delay = AMP_DELAY
+    i = delay
+    notes = []
+    while i < len(amps):
+      if(amps[i] < peak_amp/25):
+        # end note
+        # print("note end: ", i)
+        peak_amp = 0
+        notes[-1].append(start_time+i-notes[-1][0])
+      diff = amps[i] - amps[i-delay]
+      if(diff > amp_threshold):
+        n_cnt += 1
+        if(peak_amp > 0):
+          # end previous note if it's there
+          notes[-1].append(start_time+i-notes[-1][0])
+          peak_amp = 0
+        #hit start of note
+        #print("note start: ",i)
+        j = min(i + 2,len(freqs)-1)
+        while(freqs[j] < min_real_freq):
+          if j >= len(amps)-1:
+              break;
+          j += 1
+        freq = freqs[j]
+        #print("note frequency: ",freqs[j])
+        if(freq > min_real_freq):
+            notes.append([start_time+i, freqs[j]])
+            peak_amp = amps[i]
+        i += delay
+      i += 1
+    return notes
 
-print("detected ", n_cnt, " notes")
+
+
+allFreqs = numpy.zeros(0)
+allAmps = numpy.zeros(0)
+
+frames_needed = fs * 3
+freqs = numpy.zeros(0)
+amps = numpy.zeros(0)
+curtime = 0
+frame_start = 0
+allNotes = []
+with stream:
+    # keepGoing = True
+    # while keepGoing:
+    while len(allNotes) < 5:
+        detected_samples = []
+        while(len(detected_samples) < frames_needed):
+            window = q.get()
+            #print("window: ", window)
+            #print("window len: ", len(window))
+            detected_samples.extend(window)
+            #print("detected sample len: ", len(detected_samples))
+            #print("q len: ", q.qsize())
+
+
+        # fname = '/home/alrehn/dev/guitartosheet/pianoCMajor.wav'
+
+        #audio_samples = readWavSamples(fname)
+        # print("len samples: ", len(detected_samples))
+        audio_samples = numpy.array([i[0] for i in detected_samples]);
+
+        unsmoothed_freqs = autoCorFreqEstimateOverTime(audio_samples)
+        # print("len unsmoothed_freqs: ", len(unsmoothed_freqs))
+
+        newFreqs = smoothFrequencies(unsmoothed_freqs)
+        allFreqs = numpy.concatenate([allFreqs,newFreqs])#for plotting
+        freqs = numpy.concatenate([freqs,newFreqs])
+
+        newAmps = amplitudeOverTime(audio_samples)
+        allAmps = numpy.concatenate([allAmps,newAmps])#for plotting
+        amps = numpy.concatenate([amps,newAmps])
+
+        plt.savefig('freqs.png')
+
+        print("len freqs: ",len(freqs))
+        print("len amps: ",len(amps))
+
+        notes = getNotes(freqs, amps, curtime)
+        print(notes)
+
+        # cut off at the last detected note
+        # if the note is finished, cut off at the end, if it isn't, cut off at the beginning
+        if(len(notes) == 0):
+            cutoff = 0
+        else:
+            last_note = notes[-1]
+            if len(last_note) == 3:
+                cutoff = last_note[0] - curtime + last_note[2] - AMP_DELAY
+                allNotes.extend(notes)
+            else:
+                cutoff = last_note[0] - curtime - AMP_DELAY
+                allNotes.extend(notes[:len(notes)-1])
+
+        cutoff = max(0,cutoff)
+        print("cutoff: ", cutoff)
+
+        curtime = frame_start + cutoff
+        frame_start += len(freqs)
+
+        freqs = freqs[cutoff:]
+        amps = amps[cutoff:]
+
+        plt.gcf().clear()
+        plt.plot(allFreqs)
+        plt.plot(allAmps)
+        plt.savefig('freqs.png')
+
+##TEMP
+notes = allNotes
+
+# print("detected ", n_cnt, " notes")
 print(notes)
 
 ## assume first note is quarter note
 quarter = notes[0][2]
-buckets = [quarter//4,quarter//2,quarter,quarter**2,quarter**4]
+buckets = [max(quarter//4,1),max(quarter//2,1),quarter,quarter**2,quarter**4]
 #print("b: ", buckets[0], buckets[1], buckets[2])
 
 from music21 import pitch
@@ -283,7 +312,5 @@ for i in range(len(notes)):
   exportNotes.append([note_time_adj,p.name+str(p.octave),note_len_adj])
 
 print(exportNotes)
-
-plt.savefig('freqs.png')
 
 
